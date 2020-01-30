fecf47a5872e13566b263b1efbcc008a83de423a
Hrishikesh KB <hrishi.kb@gmail.com>
2020-01-30 12:35:29 +0530

    format code using black

--- transcode.py
+++ transcode.py
@@ -1,40 +1,51 @@
 #! /usr/bin/python
-''' Depends on ffmpeg and MP4Box. This scripts accepts a HD video and creates different
+""" Depends on ffmpeg and MP4Box. This scripts accepts a HD video and creates different
 bitrate versions for dash
 @author : Hrishikesh Bhaskaran <hrishi.kb@gmail.com>, May 2019
-'''
+"""
 import os
 import sys
 from xml.dom import minidom
+
 try:
     import boto3
     from datetime import datetime
 
     from dotenv import load_dotenv
+
     load_dotenv()
 except ImportError:
     print("Videos will not be uploaded to S3")
 
 config = {
-    'keyint': '59',
-    'framerate': '30000/1001',
-    'profile': 'onDemand',
-    'chunk': '1000',
+    "keyint": "59",
+    "framerate": "30000/1001",
+    "profile": "onDemand",
+    "chunk": "1000",
 }
 base_filename = None
 # pre-defined resolutions
-versions = ['256', '426', '854', '1280', '1920']
+versions = ["256", "426", "854", "1280", "1920"]
 
 files_to_clean = []
-AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
-AWS_ACCESS_SECRET_KEY = os.getenv('AWS_ACCESS_SECRET_KEY')
-bucket_name = os.getenv('S3_BUCKET_NAME')
-region = os.getenv('AWS_REGION')
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_ACCESS_SECRET_KEY = os.getenv("AWS_ACCESS_SECRET_KEY")
+bucket_name = os.getenv("S3_BUCKET_NAME")
+region = os.getenv("AWS_REGION")
+
+
 def create_multiple_bitrate_versions(filename):
     for version in versions:
         command = 'ffmpeg -i {} -vf "hqdn3d,detelecine,yadif,scale={}:-2" -x264-params "keyint={}:min-keyint={}:no-scenecut" -strict -2 -preset veryfast -crf 25 -r {} {}/{}-{} -y'.format(
-            filename, version, config.get('keyint'), config.get('keyint'),
-            config.get('framerate'), base_filename, version, filename)
+            filename,
+            version,
+            config.get("keyint"),
+            config.get("keyint"),
+            config.get("framerate"),
+            base_filename,
+            version,
+            filename,
+        )
         print(command)
         os.system(command)
 
@@ -44,31 +55,32 @@ def create_multiple_segments(filename):
     base_command = "MP4Box -dash {} -frag {} -rap -frag-rap -profile {} {} {}-{}{}"
     for version in versions:
         command = base_command.format(
-            config.get('chunk'),
-            config.get('chunk'),
-            config.get('profile'),
-            '-out ' + version + '-' + base_filename + '.mpd',
+            config.get("chunk"),
+            config.get("chunk"),
+            config.get("profile"),
+            "-out " + version + "-" + base_filename + ".mpd",
             version,
             filename,
-            '#video',
+            "#video",
         )
         print(command)
         os.system(command)
-        files_to_clean.append(base_filename + '/' + version + '-' +
-                              base_filename + '.mpd')
+        files_to_clean.append(
+            base_filename + "/" + version + "-" + base_filename + ".mpd"
+        )
     command = base_command.format(
-        config.get('chunk'),
-        config.get('chunk'),
-        config.get('profile'),
-        '-out audio.mpd',
+        config.get("chunk"),
+        config.get("chunk"),
+        config.get("profile"),
+        "-out audio.mpd",
         versions[-1:][0],
         filename,
-        '#audio',
+        "#audio",
     )
     print(command)
     os.system(command)
-    files_to_clean.append(base_filename + '/' + 'audio.mpd')
-    os.chdir('..')
+    files_to_clean.append(base_filename + "/" + "audio.mpd")
+    os.chdir("..")
 
 
 def merge_mpds():
@@ -81,34 +93,40 @@ def merge_mpds():
         current_mpd_root = minidom.parse(mpd).documentElement
         adaption_set = current_mpd_root.childNodes[3].childNodes[1]
         period_element.appendChild(adaption_set)
-    with open(base_filename + "/" + base_filename + ".mpd", 'w') as f:
+    with open(base_filename + "/" + base_filename + ".mpd", "w") as f:
         f.write(root.toxml())
+
 
 def upload_to_s3(bucket=None):
     if bucket:
         s3_client = boto3.client(
-            's3',
+            "s3",
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_ACCESS_SECRET_KEY,
-            )
+        )
         for filename in os.listdir(base_filename):
-            file = base_filename+"/"+filename
+            file = base_filename + "/" + filename
             date = datetime.date(datetime.now()).strftime("%d-%m-%Y")
             file_path = date + "/" + file
             with open(file, "rb") as f:
                 s3_client.upload_fileobj(f, bucket, file_path)
-            if filename.split('.')[-1] == "mpd":
-                print(" \033[1;32;40m https://s3.{}.amazonaws.com/{}/{}".format(region,bucket,file_path))
+            if filename.split(".")[-1] == "mpd":
+                print(
+                    " \033[1;32;40m https://s3.{}.amazonaws.com/{}/{}".format(
+                        region, bucket, file_path
+                    )
+                )
+
 
 if len(sys.argv) == 1:
     print("Enter the filename")
 else:
     filename = sys.argv[1]
-    if (' ' in filename):
-        new_name = filename.replace(" ","-")
+    if " " in filename:
+        new_name = filename.replace(" ", "-")
         os.rename(filename, new_name)
         filename = new_name
-    base_filename = filename.split('.')[0]
+    base_filename = filename.split(".")[0]
     # create output file directory
     try:
         os.mkdir(base_filename)
@@ -116,10 +134,10 @@ else:
         pass
     create_multiple_bitrate_versions(filename)
     create_multiple_segments(filename)
-    merge_mpds()  
+    merge_mpds()
     # cleanup
     for file_name in files_to_clean:
-        print('rm ' + file_name)
+        print("rm " + file_name)
         os.remove(file_name)
     # upload to s3
     upload_to_s3(bucket_name)
