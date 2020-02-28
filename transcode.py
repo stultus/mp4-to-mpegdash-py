f0b5181c61fd72ba3ad886b57996a38cb64f82d7
Hrishikesh KB <hrishi.kb@gmail.com>
2020-02-28 15:52:34 +0530

    Remove s3 upload part

--- transcode.py
+++ transcode.py
@@ -7,16 +7,6 @@ import os
 import sys
 from xml.dom import minidom
 
-try:
-    import boto3
-    from datetime import datetime
-
-    from dotenv import load_dotenv
-
-    load_dotenv()
-except ImportError:
-    print("Videos will not be uploaded to S3")
-
 config = {
     "keyint": "59",
     "framerate": "30000/1001",
@@ -28,10 +18,6 @@ base_filename = None
 versions = ["256", "426", "854", "1280", "1920"]
 
 files_to_clean = []
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-AWS_ACCESS_SECRET_KEY = os.getenv("AWS_ACCESS_SECRET_KEY")
-bucket_name = os.getenv("S3_BUCKET_NAME")
-region = os.getenv("AWS_REGION")
 
 
 def create_multiple_bitrate_versions(filename):
@@ -97,27 +83,6 @@ def merge_mpds():
         f.write(root.toxml())
 
 
-def upload_to_s3(bucket=None):
-    if bucket:
-        s3_client = boto3.client(
-            "s3",
-            aws_access_key_id=AWS_ACCESS_KEY,
-            aws_secret_access_key=AWS_ACCESS_SECRET_KEY,
-        )
-        for filename in os.listdir(base_filename):
-            file = base_filename + "/" + filename
-            date = datetime.date(datetime.now()).strftime("%d-%m-%Y")
-            file_path = date + "/" + file
-            with open(file, "rb") as f:
-                s3_client.upload_fileobj(f, bucket, file_path)
-            if filename.split(".")[-1] == "mpd":
-                print(
-                    " \033[1;32;40m https://s3.{}.amazonaws.com/{}/{}".format(
-                        region, bucket, file_path
-                    )
-                )
-
-
 if len(sys.argv) == 1:
     print("Enter the filename")
 else:
@@ -139,5 +104,3 @@ else:
     for file_name in files_to_clean:
         print("rm " + file_name)
         os.remove(file_name)
-    # upload to s3
-    upload_to_s3(bucket_name)
